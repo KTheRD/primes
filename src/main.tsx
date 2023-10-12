34f32ecd37ce8148c015ba4bb862de3bd0dacdfc
ktrd <chelovek17361999@gmail.com>
2023-10-12 23:14:46 +0300

    Fixed unwanted zoom on IPhone

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "./main.css"
 
+// if (navigator.userAgent.indexOf('iPhone') > -1) {
+//   document.querySelector("[name=viewport]")!.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1");
+// }
+//code above targets iphone safary to add maximum scale which fixes annoying zoom on input. Shouldn't change anything on other browsers, so I decided to statically add it to html
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
